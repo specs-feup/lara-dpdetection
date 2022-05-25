5955c6b68ceaa290c3a64f7bc004cdabfefbb006
hugomfandrade <hugo.mf.andrade@gmail.com>
2022-05-25 16:00:55 +0100

    pmar-t repositories, cppunit repository (3/3)

---
+++ src-repos/cppunit/src/cppunit/DllMain.cpp
@@ -0,0 +1,18 @@
+#define WIN32_LEAN_AND_MEAN 
+#define NOGDI
+#define NOUSER
+#define NOKERNEL
+#define NOSOUND
+#define BLENDFUNCTION void    // for mingw & gcc
+
+#if defined(CPPUNIT_HAVE_WIN32_DLL_LOADER)
+#include <windows.h>
+
+BOOL APIENTRY 
+DllMain( HANDLE hModule, 
+         DWORD  ul_reason_for_call, 
+         LPVOID lpReserved )
+{
+  return TRUE;
+}
+#endif
